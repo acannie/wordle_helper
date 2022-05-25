1d049431ab8adb5b8857b0419392a7894ae28f1a
acannie <acannie0530@gmail.com>
2022-05-26 01:03:25 +0900

    code: 5文字の英単語を全探索。

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-def rec(word, i, tmp, word_length):
+import string
+
+
+def rec(i, tmp, word_length):
     if i == (word_length - 1):
         print(tmp)
         return
 
-    for c in word:
-        if tmp.count(c) >= word.count(c):
-            continue
-        rec(word, i+1, tmp+c, word_length)
+    for alphabet in string.ascii_uppercase:
+        rec(i+1, tmp+alphabet, word_length)
 
 
 def main():
-    word = input()
-    word_length = len(word)
+    word_length = 5
 
-    for c in word:
-        rec(word, 0, c, word_length)
+    for alphabet in string.ascii_uppercase:
+        rec(0, alphabet, word_length)
 
 
 if __name__ == "__main__":
