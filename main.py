a833ba42923337dbb8c02a586186549d756cfaf1
acannie <acannie0530@gmail.com>
2022-05-26 01:35:52 +0900

    code: 実在する単語を抽出。

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import string
+import urllib.request
+
+url_prefix = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+
+def record_word(word):
+    url = url_prefix + word
+    req = urllib.request.Request(url)
+
+    try:
+        url = urllib.request.urlopen(req)
+    except urllib.error.URLError as e:
+        return
+
+    file_name = "ok.txt"
+    with open(file_name, "a")as f:
+        f.write(word + "\n")
 
 
 def rec(i, tmp, word_length):
     if i == (word_length - 1):
-        print(tmp)
+        record_word(tmp)
         return
 
     for alphabet in string.ascii_uppercase:
@@ -18,4 +35,6 @@ def main():
 
 
 if __name__ == "__main__":
+    record_word("test")
+    record_word("apple")
     main()
